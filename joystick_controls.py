4a20980f133be050444cc3ff66106af2d4b75b86
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-04-15 14:42:47 +0200

    Set default parameters for known joysticks

--- joystick_controls.py
+++ joystick_controls.py
@@ -59,19 +59,39 @@ mav.srcComponent = mavlink.MARSH_COMP_ID_CONTROLS
 print(f'Sending to {connection_string}')
 
 # create parameters database, all parameters are float to simplify code
-# default values for Thrustmaster T-Flight HOTAS X with yaw on throttle
 params: OrderedDict[str, float] = OrderedDict()
 params['PTCH_AXIS'] = 1.0
-params['PTCH_REVERSED'] = 1.0
+params['PTCH_REVERSED'] = 0.0
 params['ROLL_AXIS'] = 0.0
 params['ROLL_REVERSED'] = 0.0
 params['THR_AXIS'] = 2.0
-params['THR_REVERSED'] = 1.0
-params['YAW_AXIS'] = 4.0
+params['THR_REVERSED'] = 0.0
+params['YAW_AXIS'] = 3.0
 params['YAW_REVERSED'] = 0.0
 
 for k in params.keys():
     assert len(k) <= 16, 'parameter names must fit into param_id field'
+
+if 'hotas x' in device.get_name().lower():
+    print('Loading parameters for Thrustmaster T-Flight HOTAS X with yaw on throttle')
+    params['PTCH_AXIS'] = 1.0
+    params['PTCH_REVERSED'] = 1.0
+    params['ROLL_AXIS'] = 0.0
+    params['ROLL_REVERSED'] = 0.0
+    params['THR_AXIS'] = 2.0
+    params['THR_REVERSED'] = 1.0
+    params['YAW_AXIS'] = 4.0
+    params['YAW_REVERSED'] = 0.0
+elif device.get_name() == 'Arduino Leonardo':
+    print('Loading parameters for Arduino Leonardo in ATTILA simulator')
+    params['PTCH_AXIS'] = 2.0
+    params['PTCH_REVERSED'] = 1.0
+    params['ROLL_AXIS'] = 0.0
+    params['ROLL_REVERSED'] = 0.0
+    params['THR_AXIS'] = 5.0
+    params['THR_REVERSED'] = 0.0
+    params['YAW_AXIS'] = 4.0
+    params['YAW_REVERSED'] = 0.0
 
 
 def send_param(index: int, name=''):
