4bda351fda7ff8d73d21c01ea28b93e23329cd52
Andrea Zanoni <andrea.zanoni@polimi.it>
2025-10-31 14:30:19 +0100

    Implemented send of heartbeat

--- CLS/src/marshconnection.cc
+++ CLS/src/marshconnection.cc
@@ -1,5 +1,6 @@
 #include "marshconnection.h"
 #include "mavlink/c_library_v2/minimal/mavlink.h"
+#include "minimal/mavlink_msg_heartbeat.h"
 
 MarshConnection::MarshConnection(void)
 : m_marsh_socket(-1),
@@ -23,5 +24,17 @@ void MarshConnection::sendHeartbeat(void)
 {
   mavlink_heartbeat_t heartbeat;
   heartbeat.type = MAV_TYPE_GENERIC;
-  
+  heartbeat.autopilot = MAV_AUTOPILOT_INVALID;
+  heartbeat.base_mode = 0; // no flags applicable
+  heartbeat.custom_mode = 0;
+  heartbeat.system_status = MAV_STATE_ACTIVE;
+
+  mavlink_message_t message;
+  mavlink_msg_heartbeat_encode_chan(m_system_id,
+                                    m_component_id,
+                                    MAVLINK_COMM_0,
+                                    &message,
+                                    &heartbeat);
+
+  sendMessage(message);
 }
