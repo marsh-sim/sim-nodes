1dd83af7088cab343787023280f6c85337f67002
Andrea Zanoni <andrea.zanoni@polimi.it>
2025-11-07 07:56:15 +0100

    Added intervals for heartbeat and control loading communication

--- CLS/src/marsh_config.h
+++ CLS/src/marsh_config.h
@@ -1,2 +1,12 @@
+/* Warning: the frequency of the Wittenstein CLS data stream is
+ * defined in the TxList.txt file, to be uploaded in the root 
+ * directory of the CLS.
+ *
+ * Reference: WAT-MAN-CPD-1i5.0[Communication Protocol].pdf, Section 2.2.3
+ * DEFAULT_MARSH_CONTROL_LOADING_INTERVAL_MS should be defined accordingly
+ */
+
 #define DEFAULT_MARSH_MANAGER_PORT 24440
 #define DEFAULT_MARSH_MANAGER_ADDRESS "192.168.1.2"
+#define DEFAULT_MARSH_CONTROL_LOADING_INTERVAL_MS 8
+#define DEFAULT_MARSH_HEARTBEAT_INTERVAL_MS 1000
