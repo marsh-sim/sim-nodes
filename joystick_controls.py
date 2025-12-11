a02d0c565e664fa73c08fcac12f36f01e86edba9
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2025-12-11 15:41:27 +0100

    Recognise Arduino Leonardo on Ubuntu

--- joystick_controls.py
+++ joystick_controls.py
@@ -18,14 +18,22 @@ from utils import NodeFormatter
 from utils.param_dict import ParamDict
 
 parser = ArgumentParser(formatter_class=NodeFormatter, description=__doc__)
-parser.add_argument('-i', '--input-index', type=int,
-                    help='index of input device to use', default=0)
-parser.add_argument('-m', '--manager',
-                    help='MARSH Manager IP addr', default='127.0.0.1')
-parser.add_argument('-M', '--manager-port', type=int,
-                    help='MARSH Manager port', default=24400)
-parser.add_argument('-t', '--time-interval', type=float,
-                    help='interval between sent messages, in seconds', default=0.02)
+parser.add_argument(
+    "-i", "--input-index", type=int, help="index of input device to use", default=0
+)
+parser.add_argument(
+    "-m", "--manager", help="MARSH Manager IP addr", default="127.0.0.1"
+)
+parser.add_argument(
+    "-M", "--manager-port", type=int, help="MARSH Manager port", default=24400
+)
+parser.add_argument(
+    "-t",
+    "--time-interval",
+    type=float,
+    help="interval between sent messages, in seconds",
+    default=0.02,
+)
 args = parser.parse_args()
 # assign to typed variables for convenience
 args_manager: str = args.manager
@@ -35,64 +43,70 @@ args_time_interval: float = args.time_interval
 
 import pygame  # noqa - prints on import
 from pygame import joystick  # noqa - prints on import
+
 pygame.init()
 joystick.init()
 
 if joystick.get_count() == 0:
-    print('No joystick connected')
+    print("No joystick connected")
     exit(1)
 
 
 # validate arguments
 if args_input_index < 0:
-    parser.error('Input index must be non-negative')
+    parser.error("Input index must be non-negative")
 
 if args_input_index >= joystick.get_count():
     parser.error(
-        f'Input index {args_input_index} too large, only {joystick.get_count()} joysticks connected')
+        f"Input index {args_input_index} too large, only {joystick.get_count()} joysticks connected"
+    )
 
 # create joystick device
 device = joystick.Joystick(args_input_index)
 device.init()
 
 # create MAVLink connection
-connection_string = f'udpout:{args_manager}:{args_manager_port}'
+connection_string = f"udpout:{args_manager}:{args_manager_port}"
 mav = mavlink.MAVLink(mavutil.mavlink_connection(connection_string))
 mav.srcSystem = 1  # default system
-mav.srcComponent = mavlink.MAV_COMP_ID_USER1 + (mavlink.MARSH_TYPE_CONTROLS - mavlink.MARSH_TYPE_MANAGER)
-print(f'Sending to {connection_string}')
+mav.srcComponent = mavlink.MAV_COMP_ID_USER1 + (
+    mavlink.MARSH_TYPE_CONTROLS - mavlink.MARSH_TYPE_MANAGER
+)
+print(f"Sending to {connection_string}")
 
 params = ParamDict()
-params['PTCH_AXIS'] = 1.0
-params['PTCH_REVERSED'] = 0.0
-params['ROLL_AXIS'] = 0.0
-params['ROLL_REVERSED'] = 0.0
-params['THR_AXIS'] = 2.0
-params['THR_REVERSED'] = 0.0
-params['YAW_AXIS'] = 3.0
-params['YAW_REVERSED'] = 0.0
+params["PTCH_AXIS"] = 1.0
+params["PTCH_REVERSED"] = 0.0
+params["ROLL_AXIS"] = 0.0
+params["ROLL_REVERSED"] = 0.0
+params["THR_AXIS"] = 2.0
+params["THR_REVERSED"] = 0.0
+params["YAW_AXIS"] = 3.0
+params["YAW_REVERSED"] = 0.0
 
 
-if 'hotas x' in device.get_name().lower():
-    print('Loading parameters for Thrustmaster T-Flight HOTAS X with yaw on throttle')
-    params['PTCH_AXIS'] = 1.0
-    params['PTCH_REVERSED'] = 1.0
-    params['ROLL_AXIS'] = 0.0
-    params['ROLL_REVERSED'] = 0.0
-    params['THR_AXIS'] = 2.0
-    params['THR_REVERSED'] = 1.0
-    params['YAW_AXIS'] = 4.0
-    params['YAW_REVERSED'] = 0.0
-elif device.get_name() == 'Arduino Leonardo':
-    print('Loading parameters for Arduino Leonardo in ATTILA simulator')
-    params['PTCH_AXIS'] = 2.0
-    params['PTCH_REVERSED'] = 1.0
-    params['ROLL_AXIS'] = 0.0
-    params['ROLL_REVERSED'] = 0.0
-    params['THR_AXIS'] = 5.0
-    params['THR_REVERSED'] = 0.0
-    params['YAW_AXIS'] = 4.0
-    params['YAW_REVERSED'] = 0.0
+if "hotas x" in device.get_name().lower():
+    print("Loading parameters for Thrustmaster T-Flight HOTAS X with yaw on throttle")
+    params["PTCH_AXIS"] = 1.0
+    params["PTCH_REVERSED"] = 1.0
+    params["ROLL_AXIS"] = 0.0
+    params["ROLL_REVERSED"] = 0.0
+    params["THR_AXIS"] = 2.0
+    params["THR_REVERSED"] = 1.0
+    params["YAW_AXIS"] = 4.0
+    params["YAW_REVERSED"] = 0.0
+elif "Arduino Leonardo" in device.get_name():
+    print("Loading parameters for Arduino Leonardo in ATTILA simulator")
+    params["PTCH_AXIS"] = 2.0
+    params["PTCH_REVERSED"] = 1.0
+    params["ROLL_AXIS"] = 0.0
+    params["ROLL_REVERSED"] = 0.0
+    params["THR_AXIS"] = 5.0
+    params["THR_REVERSED"] = 0.0
+    params["YAW_AXIS"] = 4.0
+    params["YAW_REVERSED"] = 0.0
+else:
+    print("Using device", device.get_name(), "with default settings")
 
 
 # controlling when messages should be sent
@@ -118,7 +132,7 @@ while True:
             mavlink.MAV_AUTOPILOT_INVALID,
             mavlink.MAV_MODE_FLAG_TEST_ENABLED,
             0,
-            mavlink.MAV_STATE_ACTIVE
+            mavlink.MAV_STATE_ACTIVE,
         )
         heartbeat_next = time() + heartbeat_interval
 
@@ -126,16 +140,16 @@ while True:
         axes = [0x7FFF] * 4  # set each axis to invalid (INT16_MAX)
 
         # get joystick values based on parameters
-        for i, prefix in enumerate(['PTCH', 'ROLL', 'THR', 'YAW']):
-            axis_number = int(params[f'{prefix}_AXIS'])
+        for i, prefix in enumerate(["PTCH", "ROLL", "THR", "YAW"]):
+            axis_number = int(params[f"{prefix}_AXIS"])
             if axis_number >= 0 and axis_number < device.get_numaxes():
                 value = device.get_axis(axis_number)
-                if params[f'{prefix}_REVERSED'] != 0.0:
+                if params[f"{prefix}_REVERSED"] != 0.0:
                     value *= -1.0
                 # scale to the normalized range
                 axes[i] = round(1000 * max(-1, min(1, value)))
 
-                if prefix == 'THR':
+                if prefix == "THR":
                     # send collective between 0 and 1000
                     axes[i] = round((axes[i] + 1000) / 2)
 
@@ -147,7 +161,10 @@ while True:
 
         mav.manual_control_send(
             mav.srcSystem,
-            axes[0], axes[1], axes[2], axes[3],
+            axes[0],
+            axes[1],
+            axes[2],
+            axes[3],
             buttons,
         )
         control_next = time() + control_interval
@@ -156,11 +173,11 @@ while True:
     try:
         while (message := mav.file.recv_msg()) is not None:
             message: mavlink.MAVLink_message
-            if message.get_type() == 'HEARTBEAT':
+            if message.get_type() == "HEARTBEAT":
                 heartbeat: mavlink.MAVLink_heartbeat_message = message
                 if heartbeat.type == mavlink.MARSH_TYPE_MANAGER:
                     if not manager_connected:
-                        print('Connected to simulation manager')
+                        print("Connected to simulation manager")
                     manager_connected = True
                     manager_timeout = time() + timeout_interval
             elif params.should_handle_message(message):
@@ -171,4 +188,4 @@ while True:
 
     if manager_connected and time() > manager_timeout:
         manager_connected = False
-        print('Lost connection to simulation manager')
+        print("Lost connection to simulation manager")
